783b9e06bb9df5c09d9dc6f5cb067077b3a280ac
Mubeen Ahmad <mubeen@fingerprintsoft.co>
2016-12-15 14:24:38 +0200

    Added a Marker object to manage colour nodes based on node type

--- ng2-img-map.ts
+++ ng2-img-map.ts
@@ -2,6 +2,12 @@ import {
   Component, ElementRef, EventEmitter, Input, Output, Renderer, ViewChild
 } from '@angular/core';
 
+export interface Marker {
+  x: number;
+  y: number;
+  type?: string;
+}
+
 @Component({
   selector: 'img-map',
   styles: [
@@ -50,7 +56,7 @@ export class ImgMapComponent {
   private image: ElementRef;
 
   @Input('markers')
-  set setMarkers(markers: number[][]) {
+  set setMarkers(markers: Marker[]) {
     this.markerActive = null;
     this.markerHover = null;
     this.markers = markers;
@@ -84,7 +90,7 @@ export class ImgMapComponent {
   /**
    * Collection of markers.
    */
-  private markers: number[][] = [];
+  private markers: Marker[] = [];
 
   /**
    * Index of the hover state marker.
@@ -94,14 +100,14 @@ export class ImgMapComponent {
   /**
    * Pixel position of markers.
    */
-  private pixels: number[][] = [];
+  private pixels: Marker[] = [];
 
   /**
    * Index of the active state marker.
    */
   markerActive: number;
 
-  constructor(private renderer: Renderer) {}
+  constructor(private renderer: Renderer) { }
 
   private change(): void {
     if (this.markerActive === null) {
@@ -115,70 +121,110 @@ export class ImgMapComponent {
   /**
    * Get the cursor position relative to the canvas.
    */
-  private cursor(event: MouseEvent): number[] {
+  private cursor(event: MouseEvent): Marker {
     const rect = this.canvas.nativeElement.getBoundingClientRect();
-    return [
-      event.clientX - rect.left,
-      event.clientY - rect.top
-    ];
+    return {
+      x: event.clientX - rect.left,
+      y: event.clientY - rect.top
+    };
   }
 
   /**
    * Draw a marker.
    */
-  private drawMarker(pixel: number[], type?: string): void {
+  private drawMarker(pixel: Marker, markerState?: string, markerType?: string): void {
     const context = this.canvas.nativeElement.getContext('2d');
     context.beginPath();
-    context.arc(pixel[0], pixel[1], this.markerRadius, 0, 2 * Math.PI);
-    switch (type) {
+    context.arc(pixel.x, pixel.y, this.markerRadius, 0, 2 * Math.PI);
+
+    switch (markerState) {
       case 'active':
-        context.fillStyle = 'rgba(255, 0, 0, 0.6)';
+        context.fillStyle = 'rgba(255, 0, 0, 0.6)'; // Red
         break;
       case 'hover':
-        context.fillStyle = 'rgba(0, 0, 255, 0.6)';
+        context.fillStyle = 'rgba(0, 0, 255, 0.6)'; // Blue
         break;
       default:
-        context.fillStyle = 'rgba(0, 0, 255, 0.4)';
+        switch (markerType) {
+          case 'Advert':
+            context.fillStyle = 'rgba(0, 255, 0, 0.6)'; // Green
+            break;
+          case 'ATM':
+            context.fillStyle = 'rgb(0, 77, 0, 0.6)'; // Dark Green
+            break;
+          case 'Baby Change Room':
+            context.fillStyle = 'rgb(255, 0, 102, 0.6)'; // Magenta
+            break;
+          case 'Entrance':
+            context.fillStyle = 'rgb(102, 255, 255, 0.6)'; // Light Blue
+            break;
+          case 'Escalator':
+            context.fillStyle = 'rgb(255, 0, 255, 0.6)'; // Pink
+            break;
+          case 'Kiosk':
+            context.fillStyle = 'rgb(128, 128, 128, 0.6)'; // Grey
+            break;
+          case 'Lift':
+            context.fillStyle = 'rgb(255, 0, 255, 0.6)'; // Pink
+            break;
+          case 'Path':
+            context.fillStyle = 'rgb(255, 153, 51, 0.6)'; // Orange
+            break;
+          case 'Store':
+            context.fillStyle = 'rgba(0, 0, 0, 1)'; // Black
+            break;
+          case 'Text Point':
+            context.fillStyle = 'rrgb(102, 102, 51, 0.6)'; // Olivish
+            break;
+          case 'Toilet':
+            context.fillStyle = 'rgb(102, 51, 0, 0.6)'; // Brown
+            break;
+          default:
+            context.fillStyle = 'rgba(0, 0, 255, 0.4)'; // Blue
+        }
     }
+
     context.fill();
   }
 
   /**
    * Check if a position is inside a marker.
    */
-  private insideMarker(pixel: number[], coordinate: number[]): boolean {
+  private insideMarker(pixel: Marker, coordinate: Marker): boolean {
     return Math.sqrt(
-      (coordinate[0] - pixel[0]) * (coordinate[0] - pixel[0])
-      + (coordinate[1] - pixel[1]) * (coordinate[1] - pixel[1])
+      (coordinate.x - pixel.x) * (coordinate.x - pixel.x)
+      + (coordinate.y - pixel.y) * (coordinate.y - pixel.y)
     ) < this.markerRadius;
   }
 
   /**
    * Convert a percentage position to a pixel position.
    */
-  private markerToPixel(marker: number[]): number[] {
+  private markerToPixel(marker: Marker): Marker {
     const image: HTMLImageElement = this.image.nativeElement;
-    return [
-      (image.clientWidth / 100) * marker[0],
-      (image.clientHeight / 100) * marker[1]
-    ];
+    return {
+      x: (image.clientWidth / 100) * marker.x,
+      y: (image.clientHeight / 100) * marker.y,
+      type: marker.type
+    };
   }
 
   /**
    * Convert a pixel position to a percentage position.
    */
-  private pixelToMarker(pixel: number[]): number[] {
+  private pixelToMarker(pixel: Marker): Marker {
     const image: HTMLImageElement = this.image.nativeElement;
-    return [
-      (pixel[0] / image.clientWidth) * 100,
-      (pixel[1] / image.clientHeight) * 100
-    ];
+    return {
+      x: (pixel[0] / image.clientWidth) * 100,
+      y: (pixel[1] / image.clientHeight) * 100,
+      type: pixel.type
+    };
   }
 
   /**
    * Sets the new marker position.
    */
-  private mark(pixel: number[]): void {
+  private mark(pixel: Marker): void {
     this.markerActive = this.markers.length;
     this.markers.push(this.pixelToMarker(pixel));
     this.draw();
@@ -216,7 +262,7 @@ export class ImgMapComponent {
       } else if (this.markerHover === index) {
         this.drawMarker(pixel, 'hover');
       } else {
-        this.drawMarker(pixel);
+        this.drawMarker(pixel, pixel.type);
       }
     });
   }
